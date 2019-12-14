ec497c8ac9a18f67b23201e402ae2adb80db24e7
DVDpro <dvdpro@seznam.cz>
2019-12-14 18:51:35 +0100

    tag node and first generator (enum) csharp

---
+++ StarUML-FileFormat/Generators/EnumGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DVDpro.StarUML.FileFormat.Generators
+{
+    public class EnumGenerator
+    {
+        private string ConvertVisibility(Nodes.UmlNodeVisibility? visibility)
+        {
+            if (visibility == null) return "public";
+
+            switch (visibility.Value)
+            {
+                case Nodes.UmlNodeVisibility.Public:
+                case Nodes.UmlNodeVisibility.Protected:
+                    return "public";
+                case Nodes.UmlNodeVisibility.Package:
+                case Nodes.UmlNodeVisibility.Private:
+                    return "internal";
+                default:
+                    return "public";
+            }
+        }
+
+        public void Generate(CSharpFileStream stream, Nodes.UmlEnumerationNode enumNode)
+        {
+            if (enumNode.Tags.Any(r => r.Name == "Flags"))
+            {
+                stream.WriteCodeLine("[Flags]");
+            }
+            stream.WriteSummary(enumNode.Documentation);
+            stream.WriteCodeLine($"{ConvertVisibility(enumNode.Visibility)} enum {enumNode.Name}");
+            using (var enumScope = stream.CreateIndentScope())
+            {
+                var litCount = enumNode.Literals.Count;
+                for (var i=0; i<litCount; i++)
+                {
+                    var lit = enumNode.Literals[i];
+                    var litDelimiter = litCount > i + 1 ? "," : string.Empty;
+
+                    stream.WriteSummary(lit.Documentation);
+
+                    var valueTag = lit.Tags.FirstOrDefault(r => r.Name == "Value");
+                    if (valueTag != null)
+                    {                        
+                        stream.WriteCodeLine($"{lit.Name} = {valueTag.Value}{litDelimiter}");
+                    }
+                    else
+                    {
+                        stream.WriteCodeLine($"{lit.Name}{litDelimiter}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StarUML-FileFormat/Nodes/TagNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace DVDpro.StarUML.FileFormat.Nodes
+{
+    [NodeType(NodeTypeName)]
+    public sealed class TagNode : Node
+    {
+        private const string NodeTypeName = "Tag";
+
+        public string Value { get; set; }
+
+        public TagNode(INode parent) : base(NodeTypeName, parent)
+        {
+
+        }
+
+        private const string ValuePropertyName = "value";
+
+        public override void InitializeFromElement(JsonElement element)
+        {
+            base.InitializeFromElement(element);
+            if (element.TryGetProperty(ValuePropertyName, out var valueProperty))
+            {
+                Value = valueProperty.GetString();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer)
+        {
+            base.Write(writer);
+            if (Value != null)
+            {
+                writer.WriteString(ValuePropertyName, Value);
+            }
+        }
+    }
+}
